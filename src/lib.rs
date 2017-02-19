59f093c891cd63be3957027acdff7fc43240ae44
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-02-19 12:47:13 +0900

    Write moc code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+
+extern crate fftw3_sys as ffi;
+
+use ffi::fftw_r2r_kind;
+
+fn create_plan() {
+    let n = 128;
+    let mut in_ = vec![0.0; n];
+    let mut out = vec![0.0; n];
+    unsafe {
+        ffi::fftw_plan_r2r_1d(n as i32,
+                              in_.as_mut_ptr(),
+                              out.as_mut_ptr(),
+                              fftw_r2r_kind::FFTW_R2HC,
+                              0);
     }
 }
