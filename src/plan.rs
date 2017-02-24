aea1a0540bb0b94e8de120c339d466c2296957e6
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-02-24 22:54:32 +0900

    Plan for c64

--- src/plan.rs
+++ src/plan.rs
@@ -1,22 +1,21 @@
 
 use ffi;
 use super::r2r::*;
-use super::FLAG;
+use super::{c64, FLAG, SIGN};
 
 #[derive(Debug)]
-pub struct Plan<'a, 'b, A>
-    where A: 'a + 'b
+pub struct Plan<'a, 'b, A, B>
+    where A: 'a,
+          B: 'b
 {
     pub field: &'a mut [A],
-    pub coef: &'b mut [A],
+    pub coef: &'b mut [B],
     logical_size: usize,
     forward: ffi::fftw_plan,
     backward: ffi::fftw_plan,
 }
 
-impl<'a, 'b, A> Plan<'a, 'b, A>
-    where A: 'a + 'b
-{
+impl<'a, 'b, A, B> Plan<'a, 'b, A, B> {
     /// [field] -> [coef]
     pub fn forward(&self) {
         unsafe {
@@ -31,7 +30,7 @@ impl<'a, 'b, A> Plan<'a, 'b, A>
     }
 }
 
-impl<'a, 'b, A> Drop for Plan<'a, 'b, A> {
+impl<'a, 'b, A, B> Drop for Plan<'a, 'b, A, B> {
     fn drop(&mut self) {
         unsafe {
             ffi::fftw_destroy_plan(self.forward);
@@ -40,7 +39,7 @@ impl<'a, 'b, A> Drop for Plan<'a, 'b, A> {
     }
 }
 
-impl<'a, 'b> Plan<'a, 'b, f64> {
+impl<'a, 'b> Plan<'a, 'b, f64, f64> {
     pub fn r2r_1d(field: &'a mut [f64], coef: &'b mut [f64], kind: R2R_KIND, flag: FLAG) -> Self {
         let n = field.len();
         let forward = unsafe {
@@ -64,5 +63,33 @@ impl<'a, 'b> Plan<'a, 'b, f64> {
             forward: forward,
             backward: backward,
         }
+    }
+}
+
+impl<'a, 'b> Plan<'a, 'b, c64, c64> {
+    pub fn dft_1d(field: &'a mut [c64], coef: &'b mut [c64], sign: SIGN, flag: FLAG) -> Self {
+        let n = field.len();
+        let forward = unsafe {
+            ffi::fftw_plan_dft_1d(n as i32,
+                                  field.as_mut_ptr() as *mut ffi::fftw_complex,
+                                  coef.as_mut_ptr() as *mut ffi::fftw_complex,
+                                  sign as i32,
+                                  flag as u32)
+        };
+        let backward = unsafe {
+            ffi::fftw_plan_dft_1d(n as i32,
+                                  coef.as_mut_ptr() as *mut ffi::fftw_complex,
+                                  field.as_mut_ptr() as *mut ffi::fftw_complex,
+                                  -(sign as i32),
+                                  flag as u32)
+        };
+        Plan {
+            field: field,
+            coef: coef,
+            logical_size: n,
+            forward: forward,
+            backward: backward,
+        }
+
     }
 }
