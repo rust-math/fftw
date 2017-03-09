e9808a40833a0859d1d91d904811dd4beacebb02
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-03-09 12:53:29 +0900

    Add noramlizaiton

--- src/pair.rs
+++ src/pair.rs
@@ -5,10 +5,8 @@ use super::plan::Plan;
 use super::enums::*;
 use super::r2r::*;
 
-/// Field and Coefficient pair
-///
-/// - This struct is a wrapper of `Plan`
-/// - This struct uses fftw_malloc to enable SIMD optimization in FFTW.
+use std::ops::MulAssign;
+
 pub struct Pair<A, B> {
     pub field: RawVec<A>,
     pub coef: RawVec<B>,
@@ -27,6 +25,22 @@ impl<A, B> Pair<A, B> {
     pub fn backward(&mut self) {
         unsafe {
             self.backward.execute();
+        }
+    }
+
+    pub fn normalize_field_by(&mut self, factor: f64)
+        where A: MulAssign<f64>
+    {
+        for val in self.field.iter_mut() {
+            *val *= factor;
+        }
+    }
+
+    pub fn normalize_coef_by(&mut self, factor: f64)
+        where B: MulAssign<f64>
+    {
+        for val in self.coef.iter_mut() {
+            *val *= factor;
         }
     }
 }
@@ -50,7 +64,7 @@ impl Pair<f64, f64> {
 impl Pair<f64, c64> {
     pub fn r2c_1d(n: usize, flag: FLAG) -> Self {
         let mut field = RawVec::<f64>::new(n);
-        let mut coef = RawVec::<c64>::new(n);
+        let mut coef = RawVec::<c64>::new(n / 2 + 1);
         let forward = Plan::r2c_1d(n, &mut field, &mut coef, flag);
         let backward = Plan::c2r_1d(n, &mut coef, &mut field, flag);
         Pair {
