a8fa77840b703c3b7d36e130b89f4dd8fd6b4130
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-10 01:23:31 +0900

    Add test for 32bit interface

--- tests/r2r.rs
+++ tests/r2r.rs
@@ -2,23 +2,30 @@
 extern crate fftw;
 extern crate float_cmp;
 
+macro_rules! impl_test{
+    ($modname:ident, $float:ident, $th:expr) => {
+
+mod $modname {
+
 use fftw::*;
 use float_cmp::ApproxEqRatio;
-use std::f64::consts::PI;
+use std::$float::consts::PI;
 
 #[test]
 fn r2hc2r() {
     let n = 128;
     let mut pair = Pair::r2r_1d(n, R2R_KIND::FFTW_R2HC, FLAG::FFTW_ESTIMATE);
     for (i, val) in pair.field.iter_mut().enumerate() {
-        *val = (i + 1) as f64;
+        *val = (i + 1) as $float;
     }
     pair.forward();
     pair.backward();
-    pair.normalize_field_by(1.0 / n as f64);
+    for x in pair.field.iter_mut() {
+        *x /= n as $float;
+    }
     for (i, val) in pair.field.iter().enumerate() {
-        let ans = (i + 1) as f64;
-        if !val.approx_eq_ratio(&ans, 1e-7) {
+        let ans = (i + 1) as $float;
+        if !val.approx_eq_ratio(&ans, $th) {
             panic!("Not equal: ans={}/val={}", ans, val);
         }
     }
@@ -29,14 +36,16 @@ fn hc2r2hc() {
     let n = 128;
     let mut pair = Pair::r2r_1d(n, R2R_KIND::FFTW_R2HC, FLAG::FFTW_ESTIMATE);
     for (i, val) in pair.coef.iter_mut().enumerate() {
-        *val = (i + 1) as f64;
+        *val = (i + 1) as $float;
     }
     pair.backward();
     pair.forward();
-    pair.normalize_coef_by(1.0 / n as f64);
+    for x in pair.coef.iter_mut() {
+        *x /= n as $float;
+    }
     for (i, val) in pair.coef.iter().enumerate() {
-        let ans = (i + 1) as f64;
-        if !val.approx_eq_ratio(&ans, 1e-7) {
+        let ans = (i + 1) as $float;
+        if !val.approx_eq_ratio(&ans, $th) {
             panic!("Not equal: ans={}/val={}", ans, val);
         }
     }
@@ -49,15 +58,21 @@ fn hc2r() {
     pair.coef[0] = 2.0;
     pair.coef[1] = 1.0;
     pair.backward();
-    let ans: Vec<f64> = (0..n)
+    let ans: Vec<$float> = (0..n)
         .map(|i| {
-            let x = 2.0 * PI * i as f64 / n as f64;
+            let x = 2.0 * PI * i as $float / n as $float;
             2.0 + 2.0 * x.cos()
         })
         .collect();
     for (v, a) in pair.field.iter().zip(ans.iter()) {
-        if (v - a).abs() > 1e-7 {
+        if (v - a).abs() > $th {
             panic!("Not equal: ans={}/val={}", a, v);
         }
     }
 }
+
+} // mod
+}} // impl_test
+
+impl_test!(_32, f32, 1e-4);
+impl_test!(_64, f64, 1e-7);
