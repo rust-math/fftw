cdfc4cb855c0652d78d1c23d28f5011b91a2f373
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-20 10:48:46 +0900

    Write doc of Pair module

--- src/pair.rs
+++ src/pair.rs
@@ -1,3 +1,4 @@
+//! Safe-interface corresponding to out-place transform
 
 use super::raw_vec::*;
 use super::plan::*;
@@ -6,6 +7,13 @@ use super::r2r::*;
 
 use num_traits::Zero;
 
+//! Safe-interface corresponding to out-place transform
+//!
+//! FFTW interface modifies an array in `fftw_execute` function
+//! which does not takes the array as its arguments.
+//! It is not compatible to the programing model of safe Rust.
+//! `Pair` interface composes the array and plan to manage
+//! mutability in the safe Rust way.
 pub struct Pair<A, B> {
     pub field: RawVec<A>,
     pub coef: RawVec<B>,
@@ -17,12 +25,14 @@ pub struct Pair<A, B> {
 impl<A, B> Pair<A, B> {
     pub fn logical_size(&self) -> usize { self.logical_size }
 
+    /// Execute forward transformation
     pub fn forward(&mut self) {
         unsafe {
             self.forward.execute();
         }
     }
 
+    /// Execute backward transformation
     pub fn backward(&mut self) {
         unsafe {
             self.backward.execute();
@@ -33,6 +43,7 @@ impl<A, B> Pair<A, B> {
 impl<R> Pair<R, R>
     where R: R2RPlanCreate + AlignedAllocable + Zero
 {
+    /// Create one-dimensional Real-to-Real transformation pair
     pub fn r2r_1d(n: usize, kind: R2R_KIND, flag: FLAG) -> Self {
         let mut field = RawVec::new(n);
         let mut coef = RawVec::new(n);
@@ -51,6 +62,7 @@ impl<R> Pair<R, R>
 impl<C> Pair<C, C>
     where C: C2CPlanCreate + AlignedAllocable + Zero
 {
+    /// Create one-dimensional Complex-to-Complex transformation pair
     pub fn c2c_1d(n: usize, sign: SIGN, flag: FLAG) -> Self {
         let mut field = RawVec::new(n);
         let mut coef = RawVec::new(n);
@@ -71,6 +83,7 @@ impl<R, C> Pair<R, C>
           R: AlignedAllocable + Zero,
           C: AlignedAllocable + Zero
 {
+    /// Create one-dimensional Real-to-Complex transformation pair
     pub fn r2c_1d(n: usize, flag: FLAG) -> Self {
         let mut field = RawVec::<R>::new(n);
         let mut coef = RawVec::<C>::new(n / 2 + 1);
