e18100831cd3f5ffed1afd3d37ea11b2f4570245
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-09-10 18:45:22 +0900

    Revise example

--- examples/r2r.rs
+++ examples/r2r.rs
@@ -3,24 +3,17 @@ extern crate fftw_sys as ffi;
 extern crate fftw;
 
 use fftw::*;
-use std::f64::consts::PI;
 
 fn main() {
     let n = 128;
+    // Create a pair of array for out-place transform of FFTW
     let mut pair = Pair::r2r_1d(n, R2R_KIND::FFTW_R2HC, FLAG::FFTW_ESTIMATE);
+    // Initialize to `cos(x)` in coefficient space
     pair.coef[1] = 1.0;
+    // execute rDCT
     pair.backward();
-    for val in pair.field.iter() {
-        println!("{}", val);
-    }
-    let ans: Vec<f64> = (0..n)
-        .map(|i| {
-            let x = 2.0 * PI * i as f64 / n as f64;
-            x.cos()
-        })
-        .collect();
 
-    for val in ans.iter() {
+    for val in pair.field.iter() {
         println!("{}", val);
     }
 }
