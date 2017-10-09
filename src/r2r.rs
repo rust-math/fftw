8da17f1d970ca5955647b8ee6289e876f7dd2399
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-10-09 21:46:00 +0900

    Revert "Remove unused `logical_size`"
    This reverts commit 6381a96d9fbf77de6b35f40f4fe0c57643572e01.

--- src/r2r.rs
+++ src/r2r.rs
@@ -42,6 +42,23 @@ fn backward(kind: R2R_KIND) -> R2R_KIND {
     }
 }
 
+/// see http://www.fftw.org/fftw3_doc/Real_002dto_002dReal-Transform-Kinds.html
+fn logical_size(n: usize, kind: R2R_KIND) -> usize {
+    match kind {
+        R2R_KIND::FFTW_R2HC => n,
+        R2R_KIND::FFTW_HC2R => n,
+        R2R_KIND::FFTW_DHT => n,
+        R2R_KIND::FFTW_REDFT00 => 2 * (n - 1),
+        R2R_KIND::FFTW_REDFT01 => 2 * n,
+        R2R_KIND::FFTW_REDFT10 => 2 * n,
+        R2R_KIND::FFTW_REDFT11 => 2 * n,
+        R2R_KIND::FFTW_RODFT00 => 2 * (n + 1),
+        R2R_KIND::FFTW_RODFT01 => 2 * n,
+        R2R_KIND::FFTW_RODFT10 => 2 * n,
+        R2R_KIND::FFTW_RODFT11 => 2 * n,
+    }
+}
+
 #[derive(Debug, Clone, Copy, new)]
 pub struct R2R1D {
     n: usize,
