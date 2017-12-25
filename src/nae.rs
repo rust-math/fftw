e2c58e924c9b21a71d08a55de7114b9ba27c6fc9
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-12-26 00:17:18 +0900

    Add check if plan is NULL

--- src/nae.rs
+++ src/nae.rs
@@ -31,7 +31,7 @@ impl Shape {
     fn new(s: &[i32]) -> Self {
         Shape(s.to_vec())
     }
-    fn check<A: FFTW, B: FFTW>(&self, in_: &[A], out: &[B]) -> Result<()>{
+    fn check<A: FFTW, B: FFTW>(&self, in_: &[A], out: &[B]) -> Result<()> {
         let n = self.0.len();
         if in_.len() != n || out.len() != n {
             Err(SizeMismatchError::new().into())
@@ -51,14 +51,14 @@ pub struct C2CPlan<C: FFTW> {
 }
 
 impl<C: FFTW<Complex = C>> C2CPlan<C> {
-    pub fn new(shape: &[i32], in_: &[C], out: &[C], sign: Sign, flag: FLAG) -> Self {
-        Self {
-            plan: C::plan_c2c(&shape, in_, out, sign, flag),
+    pub fn new(shape: &[i32], in_: &[C], out: &[C], sign: Sign, flag: FLAG) -> Result<Self> {
+        Ok(Self {
+            plan: C::plan_c2c(&shape, in_, out, sign, flag)?,
             shape: Shape::new(shape),
             sign,
             flag,
             alignment: Alignment::new(in_, out),
-        }
+        })
     }
     pub fn c2c(&mut self, in_: &[C], out: &mut [C]) -> Result<()> {
         self.alignment.check(in_, out)?;
@@ -85,13 +85,13 @@ where
     C: FFTW<Real = R, Complex = C>,
     R: FFTW<Real = R, Complex = C, Plan = C::Plan>,
 {
-    pub fn new(shape: &[i32], in_: &[C], out: &[R], flag: FLAG) -> Self {
-        Self {
-            plan: C::plan_c2r(&shape, in_, out, flag),
+    pub fn new(shape: &[i32], in_: &[C], out: &[R], flag: FLAG) -> Result<Self> {
+        Ok(Self {
+            plan: C::plan_c2r(&shape, in_, out, flag)?,
             shape: Shape::new(shape),
             flag,
             alignment: Alignment::new(in_, out),
-        }
+        })
     }
     pub fn c2r(&mut self, in_: &[C], out: &mut [R]) -> Result<()> {
         self.alignment.check(in_, out)?;
@@ -118,13 +118,13 @@ where
     C: FFTW<Real = R, Complex = C>,
     R: FFTW<Real = R, Complex = C, Plan = C::Plan>,
 {
-    pub fn new(shape: &[i32], in_: &[R], out: &[C], flag: FLAG) -> Self {
-        Self {
-            plan: C::plan_r2c(&shape, in_, out, flag),
+    pub fn new(shape: &[i32], in_: &[R], out: &[C], flag: FLAG) -> Result<Self> {
+        Ok(Self {
+            plan: C::plan_r2c(&shape, in_, out, flag)?,
             shape: Shape::new(shape),
             flag,
             alignment: Alignment::new(in_, out),
-        }
+        })
     }
     pub fn r2c(&mut self, in_: &[R], out: &mut [C]) -> Result<()> {
         self.alignment.check(in_, out)?;
@@ -134,16 +134,46 @@ where
     }
 }
 
+pub trait Plan: Sized {
+    fn check_null(self) -> Result<Self>;
+}
+
+impl Plan for fftw_plan {
+    fn check_null(self) -> Result<Self> {
+        if self.is_null() {
+            Err(InvalidPlanError::new().into())
+        } else {
+            Ok(self)
+        }
+    }
+}
+
+impl Plan for fftwf_plan {
+    fn check_null(self) -> Result<Self> {
+        if self.is_null() {
+            Err(InvalidPlanError::new().into())
+        } else {
+            Ok(self)
+        }
+    }
+}
+
 /// Switch `fftw_*` and `fftwf_*`
 pub trait FFTW {
-    type Plan: Copy;
+    type Plan: Plan + Copy;
     type Real;
     type Complex;
     fn destroy_plan(Self::Plan);
     fn print_plan(Self::Plan);
-    fn plan_c2c(shape: &[i32], in_: &[Self::Complex], out: &[Self::Complex], sign: Sign, flags: FLAG) -> Self::Plan;
-    fn plan_c2r(shape: &[i32], in_: &[Self::Complex], out: &[Self::Real], flags: FLAG) -> Self::Plan;
-    fn plan_r2c(shape: &[i32], in_: &[Self::Real], out: &[Self::Complex], flags: FLAG) -> Self::Plan;
+    fn plan_c2c(
+        shape: &[i32],
+        in_: &[Self::Complex],
+        out: &[Self::Complex],
+        sign: Sign,
+        flags: FLAG,
+    ) -> Result<Self::Plan>;
+    fn plan_c2r(shape: &[i32], in_: &[Self::Complex], out: &[Self::Real], flags: FLAG) -> Result<Self::Plan>;
+    fn plan_r2c(shape: &[i32], in_: &[Self::Real], out: &[Self::Complex], flags: FLAG) -> Result<Self::Plan>;
     fn exec_c2c(p: Self::Plan, in_: &[Self::Complex], &mut [Self::Complex]);
     fn exec_c2r(p: Self::Plan, in_: &[Self::Complex], &mut [Self::Real]);
     fn exec_r2c(p: Self::Plan, in_: &[Self::Real], &mut [Self::Complex]);
@@ -167,14 +197,20 @@ impl FFTW for c64 {
     fn print_plan(p: Self::Plan) {
         excall!{ fftw_print_plan(p) };
     }
-    fn plan_c2c(shape: &[i32], in_: &[Self::Complex], out: &[Self::Complex], sign: Sign, flag: FLAG) -> Self::Plan {
-        excall!{ fftw_plan_dft(shape.len() as i32, shape.as_ptr(), in_.as_ptr() as *mut _, out.as_ptr() as *mut _, sign, flag) }
+    fn plan_c2c(
+        shape: &[i32],
+        in_: &[Self::Complex],
+        out: &[Self::Complex],
+        sign: Sign,
+        flag: FLAG,
+    ) -> Result<Self::Plan> {
+        excall!{ fftw_plan_dft(shape.len() as i32, shape.as_ptr(), in_.as_ptr() as *mut _, out.as_ptr() as *mut _, sign, flag).check_null() }
     }
-    fn plan_c2r(shape: &[i32], in_: &[Self::Complex], out: &[Self::Real], flag: FLAG) -> Self::Plan {
-        excall!{ fftw_plan_dft_c2r(shape.len() as i32, shape.as_ptr(), in_.as_ptr() as *mut _, out.as_ptr() as *mut _, flag) }
+    fn plan_c2r(shape: &[i32], in_: &[Self::Complex], out: &[Self::Real], flag: FLAG) -> Result<Self::Plan> {
+        excall!{ fftw_plan_dft_c2r(shape.len() as i32, shape.as_ptr(), in_.as_ptr() as *mut _, out.as_ptr() as *mut _, flag).check_null() }
     }
-    fn plan_r2c(shape: &[i32], in_: &[Self::Real], out: &[Self::Complex], flag: FLAG) -> Self::Plan {
-        excall!{ fftw_plan_dft_r2c(shape.len() as i32, shape.as_ptr(), in_.as_ptr() as *mut _, out.as_ptr() as *mut _, flag) }
+    fn plan_r2c(shape: &[i32], in_: &[Self::Real], out: &[Self::Complex], flag: FLAG) -> Result<Self::Plan> {
+        excall!{ fftw_plan_dft_r2c(shape.len() as i32, shape.as_ptr(), in_.as_ptr() as *mut _, out.as_ptr() as *mut _, flag).check_null() }
     }
     fn exec_c2c(p: Self::Plan, in_: &[Self::Complex], out: &mut [Self::Complex]) {
         unsafe { fftw_execute_dft(p, in_.as_ptr() as *mut _, out.as_mut_ptr()) };
