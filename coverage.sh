e2ff41375b210e836fd4b4a1b7cad9ce07bb0065
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-12-31 22:02:50 +0900

    Add coverage.sh

---
+++ coverage.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+command -v kcov > /dev/null 2>&1
+if [[ -n "$?" ]]; then
+  echo >&2 "Use system kcov"
+  kcov="kcov"
+fi
+
+if [[ -n "${TRAVIS_BUILD_DIR}" ]]; then
+  kcov_ci=${TRAVIS_BUILD_DIR}/kcov-build/usr/local/bin/kcov
+  if [[ -e ${kcov_ci} ]]; then
+    echo >&2 "Use CI kcov"
+    kcov=$kcov_ci
+  fi
+fi
+
+if [[ -z "$kcov" ]]; then
+  echo >&2 "No kcov found. aborting..."
+  exit 1
+fi
+
+for file in tests/*.rs; do
+  testname="$(basename $file)"
+  testname=${testname%.*}
+  outdir="target/cov/$testname"
+  testpath=$(ls -1 target/debug/$testname-* | head -1)
+  echo -e >&2 "\e[31m[coverage.sh]\e[m Test = ${testname}, Path = ${testpath}, cov = ${outdir}"
+  mkdir -p $outdir
+  $kcov --exclude-pattern=/.cargo,/usr/lib --verify $outdir $testpath
+done
+
+if [[ -n "${TRAVIS_BUILD_DIR}" ]]; then
+  bash <(curl -s https://codecov.io/bash)
+fi
