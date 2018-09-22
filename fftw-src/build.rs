982eeffcaa34903dcc2271f467ce0e140190a3af
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-09-22 18:31:19 +0900

    Use md5 crate

--- fftw-src/build.rs
+++ fftw-src/build.rs
@@ -1,3 +1,5 @@
+extern crate md5;
+
 use std::env::var;
 use std::fs;
 use std::io::*;
@@ -14,13 +16,11 @@ fn download(uri: &str, filename: &str, out_dir: &Path) {
     f.write(&p.stdout).unwrap();
 }
 
-fn check_sum(check_sum_path: &Path, dir: &Path) -> bool {
-    Command::new("md5sum")
-        .args(&["-c", check_sum_path.to_str().unwrap()])
-        .current_dir(dir)
-        .status()
-        .expect("Failed to check md5 sum")
-        .success()
+fn check_sum(archive: &Path) -> Result<[u8; 16]> {
+    let mut f = fs::File::open(archive)?;
+    let mut buf = Vec::new();
+    f.read_to_end(&mut buf)?;
+    Ok(md5::compute(&buf).into())
 }
 
 fn expand(archive: &Path, out_dir: &Path) {
@@ -50,11 +50,15 @@ fn build_fftw(flags: &[&str], src_dir: &Path, out_dir: &Path) {
 const FFTW: &'static str = "fftw-3.3.6-pl1";
 const ARCHIVE: &'static str = "fftw-3.3.6-pl1.tar.gz";
 const URI: &'static str = "http://www.fftw.org/fftw-3.3.6-pl1.tar.gz";
-const MD5SUM: &'static str = "fftw-3.3.6-pl1.tar.gz.md5";
+const MD5SUM: u128 = 0x682a0e78d6966ca37c7446d4ab4cc2a1;
 
-fn main() {
-    let crate_root = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap());
-    let md5sum_path = crate_root.join(MD5SUM);
+fn correct_sum() -> [u8; 16] {
+    let mut bytes = unsafe { ::std::mem::transmute::<u128, [u8; 16]>(MD5SUM) };
+    bytes.reverse();
+    bytes
+}
+
+fn main() -> Result<()> {
     let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
     let archive_path = out_dir.join(ARCHIVE);
     let src_dir = out_dir.join(FFTW);
@@ -62,7 +66,7 @@ fn main() {
     if !archive_path.exists() {
         download(URI, ARCHIVE, &out_dir);
     }
-    if !check_sum(&md5sum_path, &out_dir) {
+    if check_sum(&archive_path)? != correct_sum() {
         panic!("check sum of archive is incorrect");
     }
     expand(&archive_path, &out_dir);
@@ -81,6 +85,8 @@ fn main() {
 
     println!("cargo:rustc-link-lib=static=fftw3");
     println!("cargo:rustc-link-lib=static=fftw3f");
+
+    Ok(())
 }
 
 fn run(command: &mut Command) {
