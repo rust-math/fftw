0077c629d71b96a8d4be96b9a19c58d0084ffe47
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-04-23 01:14:54 +0900

    Refactoring UNIX installer

--- fftw-src/build.rs
+++ fftw-src/build.rs
@@ -1,91 +1,86 @@
+extern crate failure;
 extern crate md5;
+extern crate reqwest;
 
+use failure::*;
 use std::env::var;
 use std::fs;
 use std::io::*;
 use std::path::*;
 use std::process::Command;
 
-fn download(uri: &str, filename: &str, out_dir: &Path) {
-    let out = out_dir.join(filename);
-    let mut f = BufWriter::new(fs::File::create(out).unwrap());
-    let p = Command::new("curl")
-        .arg(uri)
-        .output()
-        .expect("Failed to start download");
-    f.write(&p.stdout).unwrap();
-}
+fn download_archive_unix(out_dir: &Path) -> Fallible<()> {
+    const FFTW: &'static str = "fftw-3.3.6-pl1";
+    const MD5SUM: &'static str = "682a0e78d6966ca37c7446d4ab4cc2a1";
 
-fn check_sum(archive: &Path) -> Result<[u8; 16]> {
-    let mut f = fs::File::open(archive)?;
-    let mut buf = Vec::new();
-    f.read_to_end(&mut buf)?;
-    Ok(md5::compute(&buf).into())
-}
-
-fn expand(archive: &Path, out_dir: &Path) {
-    let st = Command::new("tar")
-        .args(&["xvf", archive.to_str().unwrap()])
-        .current_dir(&out_dir)
-        .status()
-        .expect("Failed to start expanding archive");
-    if !st.success() {
-        panic!("Failed to expand archive");
+    if out_dir.join("lib/libfftw3.a").exists() && out_dir.join("lib/libfftw3f.a").exists() {
+        return Ok(());
     }
+
+    // Download
+    let uri = format!("http://www.fftw.org/{}", FFTW);
+    let mut res = reqwest::get(&uri)?;
+    if !res.status().is_success() {
+        bail!(
+            "HTTP access to {} is failed with status = {}",
+            uri,
+            res.status()
+        );
+    }
+    let mut buf = Vec::new();
+    res.copy_to(&mut buf)?;
+
+    // Verify downloaded archive by md5sum
+    let md5_sum = format!("{:x}", md5::compute(&buf));
+    if md5_sum != MD5SUM {
+        bail!(
+            "md5sum of downloaded archive is different: actual={}, correct={}",
+            md5_sum,
+            MD5SUM
+        );
+    }
+
+    // Write down to archive
+    let archive_file = out_dir.join(format!("{}.tar.gz", FFTW));
+    let mut f = BufWriter::new(fs::File::create(&archive_file)?);
+    f.write(&buf)?;
+
+    // Expand
+    let st = Command::new("tar")
+        .arg("xf")
+        .arg(&archive_file)
+        .current_dir(&out_dir)
+        .status()?;
+    if !st.success() {
+        bail!("Failed to expand archive");
+    }
+
+    // Build FFTW
+    let archive_dir = out_dir.join(FFTW);
+    build_fftw(&["--enable-single"], &archive_dir, &out_dir);
+    build_fftw(&[], &archive_dir, &out_dir);
+    Ok(())
 }
 
 fn build_fftw(flags: &[&str], src_dir: &Path, out_dir: &Path) {
     run(Command::new("./configure")
+        .arg("--with-pic")
+        .arg("--enable-static")
+        .arg(format!("--prefix={}", out_dir.display()))
         .args(flags)
         .current_dir(&src_dir));
     run(Command::new("make")
         .arg(format!("-j{}", var("NUM_JOBS").unwrap()))
         .current_dir(&src_dir));
-    run(Command::new("make")
-        .arg("install")
-        .arg(format!("DESTDIR={}", out_dir.display()))
-        .current_dir(&src_dir));
+    run(Command::new("make").arg("install").current_dir(&src_dir));
 }
 
-const FFTW: &'static str = "fftw-3.3.6-pl1";
-const ARCHIVE: &'static str = "fftw-3.3.6-pl1.tar.gz";
-const URI: &'static str = "http://www.fftw.org/fftw-3.3.6-pl1.tar.gz";
-const MD5SUM: u128 = 0x682a0e78d6966ca37c7446d4ab4cc2a1;
-
-fn correct_sum() -> [u8; 16] {
-    let mut bytes = unsafe { ::std::mem::transmute::<u128, [u8; 16]>(MD5SUM) };
-    bytes.reverse();
-    bytes
-}
-
-fn main() -> Result<()> {
+fn main() -> Fallible<()> {
     let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
-    let archive_path = out_dir.join(ARCHIVE);
-    let src_dir = out_dir.join(FFTW);
-
-    if !archive_path.exists() {
-        download(URI, ARCHIVE, &out_dir);
-    }
-    if check_sum(&archive_path)? != correct_sum() {
-        panic!("check sum of archive is incorrect");
-    }
-    expand(&archive_path, &out_dir);
-
-    build_fftw(
-        &["--enable-static", "--with-pic", "--enable-single"],
-        &src_dir,
-        &out_dir,
-    );
-    build_fftw(&["--enable-static", "--with-pic"], &src_dir, &out_dir);
-
-    println!(
-        "cargo:rustc-link-search={}",
-        out_dir.join("usr/local/lib").display()
-    );
-
+    download_archive_unix(&out_dir)?;
+    println!("cargo:rustc-link-search={}", out_dir.join("lib").display());
     println!("cargo:rustc-link-lib=static=fftw3");
     println!("cargo:rustc-link-lib=static=fftw3f");
-
     Ok(())
 }
 
