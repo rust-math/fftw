5af1d18551455f944b8f72278308ea35c54afcce
termoshtt <toshiki.teramura@gmail.com>
2019-05-01 22:15:36 +0900

    Download windows binary

--- fftw-src/build.rs
+++ fftw-src/build.rs
@@ -1,6 +1,9 @@
 extern crate failure;
 extern crate md5;
 extern crate reqwest;
+extern crate zip;
+extern crate ftp;
+extern crate cc;
 
 use failure::*;
 use std::env::var;
@@ -8,6 +11,42 @@ use std::fs;
 use std::io::*;
 use std::path::*;
 use std::process::Command;
+use zip::ZipArchive;
+
+fn download_archive_windows(out_dir: &Path) -> Fallible<()> {
+    if out_dir.join("libfftw3.dll").exists() && out_dir.join("libfftw3f.dll").exists() {
+        return Ok(());
+    }
+
+    let archive = out_dir.join("fftw_windows.zip");
+    if !archive.exists() {
+        // Download
+        let mut conn = ftp::FtpStream::connect("ftp.fftw.org:21")?;
+        conn.login("anonymous", "anonymous")?;
+        conn.cwd("pub/fftw")?;
+        let buf = conn.simple_retr("fftw-3.3.5-dll64.zip")?.into_inner();
+        // TODO calc checksum
+        let mut f = fs::File::create(&archive)?;
+        f.write(&buf)?;
+    }
+    let f = fs::File::open(&archive)?;
+    let mut zip = ZipArchive::new(f)?;
+    let target = var("TARGET").unwrap();
+    for name in &["fftw3-3", "fftw3f-3"] {
+        for ext in &["dll", "def"] {
+            let filename = format!("lib{}.{}", name, ext);
+            let mut zf =  zip.by_name(&filename)?;
+            let mut f = fs::File::create(out_dir.join(filename))?;
+            copy(&mut zf, &mut f)?;
+        }
+        run(cc::windows_registry::find_tool(&target, "lib.exe").unwrap().to_command()
+            .arg("/MACHINE:X64")
+            .arg(format!("/DEF:lib{}.def", name))
+            .arg(format!("/OUT:lib{}.lib", name))
+            .current_dir(out_dir))
+    }
+    Ok(())
+}
 
 fn download_archive_unix(out_dir: &Path) -> Fallible<()> {
     const FFTW: &'static str = "fftw-3.3.6-pl1";
@@ -75,15 +114,6 @@ fn build_fftw(flags: &[&str], src_dir: &Path, out_dir: &Path) {
     run(Command::new("make").arg("install").current_dir(&src_dir));
 }
 
-fn main() -> Fallible<()> {
-    let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
-    download_archive_unix(&out_dir)?;
-    println!("cargo:rustc-link-search={}", out_dir.join("lib").display());
-    println!("cargo:rustc-link-lib=static=fftw3");
-    println!("cargo:rustc-link-lib=static=fftw3f");
-    Ok(())
-}
-
 fn run(command: &mut Command) {
     println!("Running: {:?}", command);
     match command.status() {
@@ -96,4 +126,20 @@ fn run(command: &mut Command) {
             panic!("failed to execute `{:?}`: {}", command, error);
         }
     }
+}
+
+fn main() -> Fallible<()> {
+    let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
+    if cfg!(target_os = "windows") {
+        download_archive_windows(&out_dir)?;
+        println!("cargo:rustc-link-search={}", out_dir.display());
+        println!("cargo:rustc-link-lib=libfftw3-3");
+        println!("cargo:rustc-link-lib=libfftw3f-3");
+    } else {
+        download_archive_unix(&out_dir)?;
+        println!("cargo:rustc-link-search={}", out_dir.join("lib").display());
+        println!("cargo:rustc-link-lib=static=fftw3");
+        println!("cargo:rustc-link-lib=static=fftw3f");
+    }
+    Ok(())
 }
