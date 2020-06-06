eab3d2bafffc6131d4caa1ce0fb1a0ead3a5b42f
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-06-06 16:44:11 +0900

    Install intel-mkl using apt

---
+++ intel-mkl-apt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eux
+
+apt update
+apt install -y curl gnupg
+
+curl -sSf https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB | apt-key add -
+curl -sSf https://apt.repos.intel.com/setup/intelproducts.list -o /etc/apt/sources.list.d/intelproducts.list
+echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list
+
+apt update
+apt install -y intel-mkl-2020.1-102
