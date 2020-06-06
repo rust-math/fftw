1d2e53d81116da95790a768d7185146a3a5ceeac
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-06-06 17:12:38 +0900

    Add pkg-config

--- intel-mkl-apt.sh
+++ intel-mkl-apt.sh
@@ -2,7 +2,7 @@
 set -eux
 
 apt update
-apt install -y curl gnupg
+apt install -y curl gnupg pkg-config
 
 curl -sSf https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB | apt-key add -
 curl -sSf https://apt.repos.intel.com/setup/intelproducts.list -o /etc/apt/sources.list.d/intelproducts.list
