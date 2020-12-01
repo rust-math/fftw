53e1a5c2a8a8f4cb22eeadcba61453bdaffccba3
songww <sww47181678@163.com>
2020-12-01 23:55:59 +0800

    Add build support for iOS

--- fftw-src/build.rs
+++ fftw-src/build.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use std::env::var;
+use std::env::{var, set_var};
 use std::fs::{canonicalize, File};
 use std::io::{copy, Write};
 use std::path::{Path, PathBuf};
@@ -38,12 +38,12 @@ fn download_archive_windows(out_dir: &Path) -> Result<()> {
             .arg("/MACHINE:X64")
             .arg(format!("/DEF:lib{}.def", name))
             .arg(format!("/OUT:lib{}.lib", name))
-            .current_dir(out_dir))
+            .current_dir(out_dir));
     }
     Ok(())
 }
 
-fn build_unix(out_dir: &Path) {
+fn build_unix(out_dir: &Path, flags: &[&str]) {
     let src_dir = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap()).join("fftw-3.3.8");
     let out_src_dir = out_dir.join("src");
     fs_extra::dir::copy(
@@ -60,10 +60,12 @@ fn build_unix(out_dir: &Path) {
     )
     .unwrap();
     if !out_dir.join("lib/libfftw3.a").exists() {
-        build_fftw(&[], &out_src_dir, &out_dir);
+        build_fftw(flags, &out_src_dir, &out_dir);
     }
     if !out_dir.join("lib/libfftw3f.a").exists() {
-        build_fftw(&["--enable-single"], &out_src_dir, &out_dir);
+        let mut flags = flags.to_vec();
+        flags.push("--enable-single");
+        build_fftw(&flags, &out_src_dir, &out_dir);
     }
 }
 
@@ -83,14 +85,15 @@ fn build_fftw(flags: &[&str], src_dir: &Path, out_dir: &Path) {
     run(Command::new("make").arg("install").current_dir(&src_dir));
 }
 
-fn run(command: &mut Command) {
+fn run(command: &mut Command) -> String {
     println!("Running: {:?}", command);
-    match command.status() {
-        Ok(status) => {
-            if !status.success() {
-                panic!("`{:?}` failed: {}", command, status);
+    match command.output() {
+        Ok(output) => {
+            if !output.status.success() {
+                panic!("`{:?}` failed: {}", command, output.status);
             }
-        }
+            return String::from_utf8(output.stdout).unwrap()
+        },
         Err(error) => {
             panic!("failed to execute `{:?}`: {}", command, error);
         }
@@ -98,16 +101,44 @@ fn run(command: &mut Command) {
 }
 
 fn main() {
+    println!("cargo:rerun-if-cahnged=build.rs");
     let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
-    if cfg!(target_os = "windows") {
-        download_archive_windows(&out_dir).unwrap();
-        println!("cargo:rustc-link-search={}", out_dir.display());
-        println!("cargo:rustc-link-lib=libfftw3-3");
-        println!("cargo:rustc-link-lib=libfftw3f-3");
-    } else {
-        build_unix(&out_dir);
-        println!("cargo:rustc-link-search={}", out_dir.join("lib").display());
-        println!("cargo:rustc-link-lib=static=fftw3");
-        println!("cargo:rustc-link-lib=static=fftw3f");
+    let target = var("TARGET").unwrap();
+    let triple = target.split("-").collect::<Vec<_>>();
+    let target_os = var("CARGO_CFG_TARGET_OS").unwrap();
+    let arch = match triple[0] {
+        "aarch64" => "arm64",
+        "armv7" => "armv7",
+        "armv7s" => "armv7s",
+        "x86" => "x86",
+        "x86_64" => "x86_64",
+        &_ => panic!("Unsupported platform {}", target_os),
+    };
+    match target_os.as_ref() {
+        "ios" => {
+            let cc = run(Command::new("xcrun").args(&["--sdk", "iphoneos", "-f", "clang"]));
+            set_var("CC", cc.trim());
+            let sysroot = run(Command::new("xcrun").args(&["--sdk", "iphoneos", "--show-sdk-path"]));
+            set_var("CFLAGS", format!("-O3 -pipe -isysroot {} -arch {} -miphoneos-version-min=9.0", sysroot.trim(), arch));
+            let flags = &[&format!("--with-sysroot={}", sysroot.trim()), "--host=arm-apple-darwin"];
+            build_unix(&out_dir, flags);
+        }
+        "android" => {
+        },
+        "windows" => {
+            download_archive_windows(&out_dir).unwrap();
+            println!("cargo:rustc-link-search={}", out_dir.display());
+            println!("cargo:rustc-link-lib=libfftw3-3");
+            println!("cargo:rustc-link-lib=libfftw3f-3");
+        }
+        _ => {
+            if var("CARGO_CFG_TARGET_FAMILY").unwrap_or("".to_string()) != "unix" {
+                panic!("Unsupported platform {}", target_os);
+            }
+            build_unix(&out_dir, &[]);
+            println!("cargo:rustc-link-search={}", out_dir.join("lib").display());
+            println!("cargo:rustc-link-lib=static=fftw3");
+            println!("cargo:rustc-link-lib=static=fftw3f");
+        }
     }
 }
