2b3644218e5a527b6ebf5bc0004cf75f2a753748
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-04-29 17:12:34 +0900

    Use static linking in fftw-sys

--- fftw-sys/src/lib.rs
+++ fftw-sys/src/lib.rs
@@ -10,9 +10,9 @@ use libc::FILE;
 pub use num_complex::Complex32 as fftwf_complex;
 pub use num_complex::Complex64 as fftw_complex;
 
-#[cfg_attr(feature = "system", link(name = "fftw3"))]
+#[cfg_attr(feature = "system", link(name = "fftw3", kind = "static"))]
 extern "C" {}
-#[cfg_attr(feature = "system", link(name = "fftw3f"))]
+#[cfg_attr(feature = "system", link(name = "fftw3f", kind = "static"))]
 extern "C" {}
 
 include!("fftw.rs");
